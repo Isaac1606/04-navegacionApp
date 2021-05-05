f07d6a41417086b3d4c66af31cfe45e41ed41b6e
Isaac1606 <isaac-dx@live.com.mx>
2021-05-05 18:37:14 -0500

    V1.0.0
    Primer Version

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import { SafeAreaView, Text, View } from 'react-native';
 
 export const App = () => {
   return (
-    <SafeAreaView>
+    <SafeAreaView> 
       <View>
         <Text>
           Hello Wold!
