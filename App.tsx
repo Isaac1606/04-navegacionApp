4954e87f9490731eaf666dc03d1a527a9d8d4590
Isaac <isaac-dx@live.com.mx>
2021-05-05 15:18:18 -0500

    first commit

---
+++ App.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SafeAreaView, Text, View } from 'react-native';
+
+export const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text>
+          Hello Wold!
+        </Text>
+      </View>
+    </SafeAreaView>
+  )
+}
+
+
+ export default App;
